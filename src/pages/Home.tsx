faaebe04ba7847dd9734b62556ed4c1113f5e829
Danx <danxgaming69@gmail.com>
2023-10-03 13:03:26 +0200

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div className="relative">
+      <div className="spacer layer1">
+        <div className="flex flex-col items-center justify-center h-screen text-5xl font-semibold text-white gap-11">
+          <p className="text-[#002233]">HELLO! MY NAME IS</p>
+          <h1 className=" drop-shadow-[4px_4px_0px_#0066FF] text-white text-9xl">
+            DANIEL ALIPHON
+          </h1>
+          <p className=" text-[#0066FF]">AND THIS IS MY PORTFOLIO</p>
+        </div>
+      </div>
+    </div>
+  );
+};
